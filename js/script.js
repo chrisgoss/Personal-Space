a4e38e6dca0051a00f7516af09ffaa90b1d145ab
chrisgoss <christinagoss206@gmail.com>
2020-06-10 22:23:47 -0700

    bedtime

--- js/script.js
+++ js/script.js
@@ -5,7 +5,3 @@ let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
 ctx.clearRect(0, 0, 400, 600);
-
-ctx.fillStyle = "#f00";
-
-ctx.fillRect(20, 20, 100, 100);
