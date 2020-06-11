4f093692eb27472d4dde38e9305c93c929bf9a21
chrisgoss <christinagoss206@gmail.com>
2020-06-10 19:26:18 -0700

    cleaning folders

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('Hello Chris, ya dumb betch!');
+
+let canvas = document.getElementById("gameScreen");
+
+let ctx = canvas.getContext("2d");
+
+ctx.clearRect(0, 0, 400, 600);
+
+ctx.fillStyle = "#f00";
+
+ctx.fillRect(20, 20, 100, 100);
