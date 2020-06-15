a1a07adbc36a19ceb06b572d927bd86b2ce7e7b0
chrisgoss <christinagoss206@gmail.com>
2020-06-15 11:19:46 -0700

    test rocket svg

--- js/script.js
+++ js/script.js
@@ -8,14 +8,12 @@ ctx.clearRect(0, 0, 500, 500);
 
 
 
-
-
 function gameObject(x, y){
     //this.fillText = text;
     this.x = x;
     this.y = y;
     this.render = function(text){
-        ctx.font = "20px Arial"
+        ctx.font = "1.5em Arial"
         ctx.fillText(text, this.x, this.y)
     }
     this.playerMove = function(){
@@ -32,10 +30,12 @@ function gameObject(x, y){
 
 
 
-
-
-let rocket = new gameObject(100, 100)
+let rocket = new gameObject(100, 300)
     rocket.playerMove();
-    rocket.render("🚀")
-let alien = new gameObject("👽", 20, 0)
-let star = new gameObject("⭐️", 30, 30)
+    rocket.render("🚀");
+let star = new gameObject(100, 200)   
+    star.starMove();
+    star.render("⭐️");
+let alien = new gameObject(100, 100)
+    alien.alienMove();
+    alien.render("👽");
