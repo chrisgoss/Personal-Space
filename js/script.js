979dd1dff9c32d2861be44ac8edfc26e84a92587
chrisgoss <christinagoss206@gmail.com>
2020-06-15 21:12:35 -0700

    working on stopping game interval

--- js/script.js
+++ js/script.js
@@ -18,12 +18,45 @@ function gameObject(x, y, color, width, height){
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
+function alienObject(x, y, color, width, height){
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.height = height;
+    this.width = width;
+    this.alive = true;
+    this.render = function(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+        this.y = this.y + 20
+        if (this.y > gameZone.height){
+            this.y = 0
+            this.x = Math.random() * gameZone.width
+        }
+    }
+}
+function starObject(x, y, color, width, height){
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.height = height;
+    this.width = width;
+    this.alive = true;
+    this.render = function(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+        this.y = this.y + 10
+        if (this.y > gameZone.height){
+            this.y = 0
+            this.x = Math.random() * gameZone.width
+        }
+    }
+}
 
 let player = new gameObject(225, 300, "gray", 50, 50);
-let alien = new gameObject(100, 100, "green", 50, 50);
-let star = new gameObject(300, 300, "gold", 50, 50);
+let alien = new alienObject(0, 0, "green", 50, 50);
+let star = new starObject(0, 0, "gold", 50, 50);
 
-//main game loop
 const gameTick = () => {
     ctx.clearRect(0, 0, gameZone.width, gameZone.height)
     if (alien.alive){
@@ -33,20 +66,16 @@ const gameTick = () => {
     }
     player.render()
     alien.render()
+    star.render()
 }
 
-//detect hits
 const detectHit = () => {
     if(player.x + player.width > alien.x
         && player.x < alien.x + alien.width
             && player.y < alien.y + alien.height
                 && player.y + player.height > alien.y){
-                    console.log("abduction!")
+                    console.log("ABDUCTION!")
                     }
-}
-
-const checkCollide = () => {
-    console.log ("collision")
 }
 
 const endGame = () => {
@@ -56,8 +85,6 @@ const endGame = () => {
 
 let gameLoop = setInterval(gameTick, 60);
 
-//add functionality for arrow keys
-//add functionality for mobile touch recognition
 const movementHandler = (e) => {
     switch(e.key){
         case "w":
@@ -75,40 +102,3 @@ const movementHandler = (e) => {
     }
 }
 document.addEventListener("keydown", movementHandler);
-
-// let player = {
-//     x: 250,
-//     y: -300,
-//     color: "gray",
-//     width: 50,
-//     height: 50,
-//     alive: true,
-//     render: function(){
-//         ctx.fillStyle = this.color
-//         ctx.fillRect(this.x, this.y, this.width, this.height)
-//     }
-// }
-// let alien = {
-//     x: 250,
-//     y: -100,
-//     color: "green",
-//     width: 50,
-//     height: 50,
-//     alive: true,
-//     render: function(){
-//         ctx.fillStyle = this.color
-//         ctx.fillRect(this.x, this.y, this.width, this.height)
-//     }
-// }
-// let star = {
-//     x: 250,
-//     y: -200,
-//     color: "gold",
-//     width: 50,
-//     height: 50,
-//     alive: true,
-//     render: function(){
-//         ctx.fillStyle = this.color
-//         ctx.fillRect(this.x, this.y, this.width, this.height)
-//     }
-// }
